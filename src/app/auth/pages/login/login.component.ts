092adbca4850d611631f4ba47fab4962d7ce1a8c
eduardo_dorado <eduardo.dorado@sanmina.com>
2022-08-28 21:50:51 -0500

    update - Login, signup reactive form

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -8,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  myForm: FormGroup = this._fb.group({
+    email: ['eduardo@gmail.com', [Validators.required, Validators.email]],
+    password: ['123456', [Validators.required, Validators.minLength(6)]]
+  });
+
+  constructor(
+    private _fb: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  // inputValidate( inputName: string ): boolean {
+  //   return this.myForm.get(inputName)?.invalid
+  //       && this.myForm.get(inputName)?.touched || true;
+  // }
+
+  login():void {
+    if( this.myForm.invalid ) {
+      console.log('Formulario invalido');
+      return;
+    }
+    console.log(this.myForm.value);
   }
 
 }
--- src/app/auth/pages/singup/singup.component.ts
+++ src/app/auth/pages/singup/singup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-singup',
@@ -8,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SingupComponent implements OnInit {
 
-  constructor() { }
+  myForm: FormGroup = this._fb.group({
+    name: ['eduardo', [Validators.required]],
+    email: ['eduardo@gmail.com', [Validators.required, Validators.email]],
+    password: ['123456', [Validators.required, Validators.minLength(6)]]
+  });
+
+  constructor(
+    private _fb: FormBuilder
+  ) { }
 
   ngOnInit(): void {
   }
 
+  // inputValidate( inputName: string ): boolean {
+  //   return this.myForm.get(inputName)?.invalid
+  //       && this.myForm.get(inputName)?.touched || true;
+  // }
+
+  register():void {
+    if( this.myForm.invalid ) {
+      console.log('Formulario invalido');
+      return;
+    }
+    console.log(this.myForm.value);
+  }
 }
